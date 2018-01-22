f9d21c9b2e0095c2c15f7f5348d51a74862db2e0
humzahchoudry <Humzah.choudry@gmail.com>
2018-01-22 13:08:54 -0500

    began writing create controller method for game

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -3,5 +3,19 @@ class Api::V1::GamesController < ApplicationController
   def index
     render json: {"hello": "world"}
   end
-  
+
+  def create
+    #create new instance
+      #take in our name, automatically put in all of the questions
+    #return new instance of game in json format
+    Game.create(game_params)
+    byebug
+  end
+
+
+  private
+
+  def game_params()
+    params.require(:game).permit(:name)
+  end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,15 @@
 class Game < ApplicationRecord
   has_many :responses
   has_many :questions, through: :responses
+
+  def initialize(name)
+    self.name = name
+    self.score = nil
+    self.questions = get_all_questions #return an array of Questions
+    self.responses = []
+  end
+
+  def get_all_questions
+
+  end
 end
