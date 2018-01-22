5fdf20da8555abe99b2724be3b0b4afd3ecb598e
iamm28 <alisonmackay246@gmail.com>
2018-01-22 14:24:03 -0500

    fetch to start game

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,15 +1,15 @@
 class Api::V1::GamesController < ApplicationController
 
-  def index
-    render json: {"hello": "world"}
-  end
+  # def index
+  #   render json: {"hello": "world"}
+  # end
 
   def create
     #create new instance
       #take in our name, automatically put in all of the questions
     #return new instance of game in json format
-    Game.create(game_params)
-    byebug
+    @game = Game.create(game_params)
+    render json: @game
   end
 
 
--- app/models/game.rb
+++ app/models/game.rb
@@ -2,12 +2,12 @@ class Game < ApplicationRecord
   has_many :responses
   has_many :questions, through: :responses
 
-  def initialize(name)
-    self.name = name
-    self.score = nil
-    self.questions = get_all_questions #return an array of Questions
-    self.responses = []
-  end
+  # def initialize(name)
+  #   self.name = name
+  #   self.score = nil
+  #   self.questions = get_all_questions #return an array of Questions
+  #   self.responses = []
+  # end
 
   def get_all_questions
 
